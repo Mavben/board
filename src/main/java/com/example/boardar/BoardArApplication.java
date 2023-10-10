1f327f35e85279186767bbb26a599d7774045666
Mavben <evrok907@gmail.com>
2023-10-10 16:31:49 +0900

    commit

--- src/main/java/com/example/boardar/BoardArApplication.java
+++ src/main/java/com/example/boardar/BoardArApplication.java
@@ -1,18 +1,3 @@
-/*
-
-1. FrontEnd : 자유(jsp, 타임리프, 앵귤러.JS, VUE.JS, React)
-2. BackEnd : Spring boot
-             - RESTful API
-			 - Login(jwt로 구현)
-			 - Paging(더보기 안됨)
-			 - N + 1 문제 해결
-			 - Comment 기능
-			 - Attach File
-             - jpa table 3개 이상
-			 - 본문 텍스트 blob, clob로 저장
-
-*/
-
 package com.example.boardar;
 
 import org.springframework.boot.SpringApplication;
